6ffe1947b99624fbccf1b45df52e294dfd885a3c
AlexIsOpAf <XenaDatabase@Alexs-MacBook-Pro.local>
2020-02-17 21:21:25 +0000

    Started Architecture using docker compose: Started Understanding on how I want to implement my API in the best manner - using multiplexers: Need to work on dumping data from a clean mongo setup into a docker container on every build: Need to work on build files via yml/travis

--- GOAPI/cmd/api/main.go
+++ GOAPI/cmd/api/main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"github.com/AlexIsOpAf/dofusplanner.io/equipment"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
 
 func main() {
-	fmt.Println("Hello world 2")
+
+	/* READ IN DB OPTIONS FROM ENVIRONMENT */
+		// GET DB
+
+	/* START WEB SERVER with address hosted on environment - development or production */
+		/* IF DEVELOPMENT SERVER - RUN DOCKER ISH*/
+		/* IF DOCKER IS NOT RUNNING - BREAK */
+
+	/* START */
+	r := mux.NewRouter()
+	r.HandleFunc("/equipment/{type}", equipment.EquipmentHandler)
+
+	http.Handle("/", r)
+	r.Use(loggingMiddleware)
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func loggingMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func (w http.ResponseWriter, r* http.Request) {
+		log.Println(r.RequestURI)
+		next.ServeHTTP(w,r)
+	})
 }
--- GOAPI/go.mod
+++ GOAPI/go.mod
@@ -2,10 +2,4 @@ module github.com/AlexIsOpAf/dofusplanner.io
 
 go 1.12
 
-require (
-	github.com/google/pprof v0.0.0-20200212024743-f11f1df84d12 // indirect
-	golang.org/x/arch v0.0.0-20191126211547-368ea8f32fff // indirect
-	golang.org/x/crypto v0.0.0-20200214034016-1d94cc7ab1c6 // indirect
-	golang.org/x/sys v0.0.0-20200212091648-12a6c2dcc1e4 // indirect
-	golang.org/x/tools v0.0.0-20200216192241-b320d3a0f5a2 // indirect
-)
+require github.com/gorilla/mux v1.7.4
